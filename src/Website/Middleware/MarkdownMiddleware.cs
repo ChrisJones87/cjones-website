3f8ec13eb1f65c01863ce80947429516c99387f7
Christopher Jones <chris.jones@wessexlabels.co.uk>
2020-02-13 08:17:27 +0000

    Blog post page showing markdown from post file

---
+++ src/Website/Middleware/MarkdownMiddleware.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+
+namespace Website.Middleware
+{
+   public class MarkdownMiddleware
+   {
+      private readonly RequestDelegate _next;
+
+      public MarkdownMiddleware(RequestDelegate next)
+      {
+         _next = next;
+      }
+
+      public Task Invoke(HttpContext httpContext)
+      {
+         var markdownText = "  # Blog Post Header\nThis is a blog post! \n* point 1\n* point 2\n* point 3";
+
+         var html = Markdig.Markdown.ToHtml(markdownText);
+
+         return _next(httpContext);
+      }
+   }
+
+   public static class MarkdownMiddlewareExtensions
+   {
+      public static IApplicationBuilder UseMarkdown(this IApplicationBuilder builder)
+      {
+         return builder.UseMiddleware<MarkdownMiddleware>();
+      }
+   }
+}
---
+++ src/Website/Pages/Blog/Post.cshtml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.FileProviders;
+using Microsoft.Extensions.Logging;
+
+namespace Website.Pages.Blog
+{
+   public class PostModel : PageModel
+   {
+      private readonly IWebHostEnvironment _environment;
+      private readonly ILogger<PostModel> _logger;
+
+      public PostModel(IWebHostEnvironment environment, ILogger<PostModel> logger)
+      {
+         _environment = environment;
+         _logger = logger;
+      }
+
+      public string PostText { get; private set; }
+      public string PostTitle { get; private set; }
+
+      private string GeneratePostTitle(int year, int month, string postName)
+      {
+         var name = postName.Replace("_", " ");
+
+         return $"{year:D4}-{year:D2} - {name}";
+      }
+
+      public async Task<IActionResult> OnGetAsync(int year, int month, string postName)
+      {
+         try
+         {
+            var filename = $"posts/{year:D4}/{month:D2}/{postName}.md";
+            var file = _environment.WebRootFileProvider.GetFileInfo(filename);
+
+            if (!file.Exists)
+               return NotFound();
+
+            await using var stream = file.CreateReadStream();
+            using var reader = new StreamReader(stream);
+
+            PostText = await reader.ReadToEndAsync();
+            PostTitle = GeneratePostTitle(year, month, postName);
+
+            return Page();
+         }
+         catch (Exception ex)
+         {
+            _logger.LogError(ex, "Failed to display blog post");
+            return BadRequest();
+         }
+      }
+   }
+}
