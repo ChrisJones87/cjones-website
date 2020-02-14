2d3af49e49070b1ff29acf492a038b344f3a2539
Christopher Jones <chris.jones@wessexlabels.co.uk>
2020-02-14 09:02:45 +0000

    Blog filtering WIP

--- src/Website/Pages/Blog/Index.cshtml.cs
+++ src/Website/Pages/Blog/Index.cshtml.cs
@@ -15,6 +15,7 @@ namespace Website.Pages.Blog
       private readonly IWebHostEnvironment _environment;
       private readonly ILogger _logger;
       private readonly BlogConfiguration _blogConfiguration;
+      private List<BlogPostMetadata> _blogPosts;
 
       public IndexModel(IWebHostEnvironment environment,
                         ILogger<IndexModel> logger,
@@ -25,15 +26,29 @@ namespace Website.Pages.Blog
          _blogConfiguration = blogConfigurationOptions.Value;
       }
 
-      public List<BlogPostMetadata> BlogPosts => _blogConfiguration.Posts;
+      public List<BlogPostMetadata> BlogPosts => _blogPosts;
 
       public List<string> Categories { get; set; }
       public List<string> Tags { get; set; }
 
       public async Task<IActionResult> OnGetAsync([FromQuery] string category, [FromQuery] string tag)
       {
-         Categories = BlogPosts.Where(x => !string.IsNullOrWhiteSpace(x.Category)).Select(x => x.Category).Distinct().ToList();
-         Tags = BlogPosts.Where(x => x.Tags != null).SelectMany(x => x.Tags).Distinct().ToList();
+         Categories = _blogConfiguration.Posts.Where(x => !string.IsNullOrWhiteSpace(x.Category)).Select(x => x.Category).Distinct().ToList();
+         Tags = _blogConfiguration.Posts.Where(x => x.Tags != null).SelectMany(x => x.Tags).Distinct().ToList();
+
+         var query = _blogConfiguration.Posts.AsQueryable();
+
+         if (!string.IsNullOrWhiteSpace(category))
+         {
+            query = query.Where(x => x.Category == category);
+         }
+
+         if (!string.IsNullOrWhiteSpace(tag))
+         {
+            query = query.Where(x => x.Tags.Contains(tag));
+         }
+
+         _blogPosts = query.ToList();
 
          return Page();
       }
